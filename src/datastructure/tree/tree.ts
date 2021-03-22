cb6a20c8102ff39e55b8a9b1c0f9d45cb360df6b
Felipe Miranda <felip.simi@gmail.com>
2021-03-21 23:02:24 -0300

    Refactor: export standard

--- src/datastructure/tree/tree.ts
+++ src/datastructure/tree/tree.ts
@@ -1,7 +1,9 @@
 import { Comparable } from '../../util/comparable';
 
-export interface Tree<T extends Comparable<T>> {
+interface Tree<T extends Comparable<T>> {
   contains(value: T): boolean;
   preOrderTraversal(): Array<T>;
   put(value: T): void;
 }
+
+export { Tree };
